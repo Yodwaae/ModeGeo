a3eddc01b0c2b14a536f59aebe983004c56f2f96
Lucas SPITZ DREVET | Yodwaae <lucas.spitz.drevet@gmail.com>
2025-11-17 16:39:47 +0100

    Sphere in voxel first draft
    - Still a lot of problems in the function

--- Assets/Scripts/VoxelShape.cs
+++ Assets/Scripts/VoxelShape.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class VoxelShape : MonoBehaviour
 {
+    [Header("Shape values")]
+    public float sphereRadius;
+    // TODO Could add a sphereCenter that set to the root node pos
+
     [Header("Tree values")]
     private OctreeNode treeRoot;
     [SerializeField] private int desiredDepth;
@@ -28,16 +32,23 @@ public class VoxelShape : MonoBehaviour
 
     private void RenderVoxelShape(OctreeNode parentNode)
     {
-        for (int i = 0; i < parentNode.children.Count(); i++) {
+        // Loop through all children
+        for (int i = 0; i < parentNode.children.Length; i++) {
             OctreeNode childNode = parentNode.children[i];
 
+            // Compute the scale and position of the node
             float factor = 1 / Mathf.Pow(2, childNode.depth);
-            childNode.mPosition = parentNode.mPosition + (posArray[i] * factor);
-            childNode.mScale = parentNode.mScale * .5f;
+            childNode.position = parentNode.position + (posArray[i] * factor);
+            childNode.scale = parentNode.scale * .5f;
 
-            if (childNode.isLeaf)
+            // TODO Improve the function : should take directly the node as argument and test wheter it's completely inside, partially inside or not inside at all
+            isInsideSphere(childNode);
+
+            // If the node is a leaf or is full create the mesh
+            // Else if the node is not fully empty, create it's children recursively
+            if (childNode.isLeaf && childNode.isFull) // TODO Right now it's an && because isFull is partially implemanted, but in the future isLeaf will be removed from the cond
                 SpawnMesh(childNode);
-            else
+            else if (!childNode.isEmpty && !childNode.isLeaf) // TODO should I just skip leaf at the start of the function it would be less aribtrary than putting that here
                 RenderVoxelShape(childNode);
 
         }
@@ -51,14 +62,18 @@ public class VoxelShape : MonoBehaviour
         MeshFilter filter = obj.AddComponent<MeshFilter>();
         MeshRenderer renderer = obj.AddComponent<MeshRenderer>();
 
-
         // Set the voxel mesh and material
         filter.sharedMesh = cube;
         renderer.sharedMaterial = material;
 
         // Scale and place
-        obj.transform.localScale = node.mScale;
-        obj.transform.position = node.mPosition;
+        obj.transform.localScale = node.scale;
+        obj.transform.position = node.position;
         
+    }
+
+    private void isInsideSphere(OctreeNode node) 
+    { 
+        node.isFull = node.position.sqrMagnitude <= sphereRadius * sphereRadius; 
     }
 }
